61d29959e10a8638292b73b4d0526e05fb70b13d
Seojeong Kim <sadiekim95@gmail.com>
2022-01-28 11:04:53 +1300

    Code copied from code-from-class to play around

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,23 +1,32 @@
 import React, { useState } from 'react'
-import { Routes } from 'react-router-dom'
-import { Route} from 'react-router-dom'
-import React from 'react'
-// import Pixel from './Pixel'
+import { Routes, Route, Link } from 'react-router-dom'
+import Details from './Details'
 import Form from './Form'
+import List from './List'
 
 const data = [
-  {name: Sadie}
+  { id: 1, product: 'Banana', price: 3.59, notes: 'You can eat it or use it as a placeholder in your code' },
+  { id: 2, product: 'Turmeric', price: 4.68, notes: 'Super healthy, get it from Bin Inn' },
+  { id: 3, product: 'Beans', price: 1.5, notes: 'Buy in bulk if you have time to cook it, or canned from PakNSave' }
 ]
 
 function App () {
+  console.log('App rendered')
   const [list, setList] = useState(data)
   return (
-<>
-<Routes>
-    <Route path ='/' element ={<Form setList = {setList} />} /> 
-</Routes> 
-</>   
-
+    <main className='app'>
+      <header className='title'>
+        <Link to={'/'}><h1>My Todos App</h1></Link>
+      </header>
+      <Link to='/create'>➕ Add</Link>
+      <section className='page'>
+        <Routes>
+          <Route path='/' element={<List products={list} />} />
+          <Route path='/create' element={<Form setList={setList} />} />
+          <Route path='/details/:id' element={<Details products={list} />} />
+        </Routes>
+      </section>
+    </main>
   )
 }
 
